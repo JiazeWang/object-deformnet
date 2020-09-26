36bd2557ad781b7174a7f8736ef43ded921ec429
JiazeWang <jiazewang@csu.edu.cn>
2020-09-26 15:17:47 +0800

    save

--- mean_shape.py
+++ mean_shape.py
@@ -106,7 +106,7 @@ for i in range(opt.n_cat):
     # save point cloud and visualize
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(mean_shape)
-    visualize_shape(catId_to_name[i], [pcd], opt.result_dir)
+    #visualize_shape(catId_to_name[i], [pcd], opt.result_dir)
 # save results
 np.save(os.path.join(opt.result_dir, 'mean_embedding'), mean_emb)
 np.save(os.path.join(opt.result_dir, 'mean_points_emb'), mean_points)
