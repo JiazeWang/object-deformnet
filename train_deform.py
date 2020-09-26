fa00b7bc3e2178a4b71f6136fcc66a1f2c4e91f5
JiazeWang <jiazewang@csu.edu.cn>
2020-09-27 00:19:05 +0800

    fix

--- train_deform.py
+++ train_deform.py
@@ -150,7 +150,7 @@ def train_net():
         #                                         num_workers=opt.num_workers, pin_memory=True)
         val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=1, sampler=val_sampler,
                                                  num_workers=opt.num_workers, pin_memory=True)
-        estimator.eval()#
+        estimator.eval()
         for i, data in enumerate(val_dataloader, 1):
             points, rgb, choose, cat_id, model, prior, sRT, nocs = data
             points = points.cuda()
