de44fb2ffbc849489759fba6733c3ae347fbbf63
JiazeWang <jiazewang@csu.edu.cn>
2020-10-12 15:25:21 +0800

    Update network_t2.py

--- lib/network_t2.py
+++ lib/network_t2.py
@@ -33,7 +33,29 @@ class DeformNet(nn.Module):
             nn.Conv1d(64, 64, 1),
             nn.ReLU(),
         )
-        self.instance_global = nn.Sequential(
+        self.instance_global0 = nn.Sequential(
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+        )
+
+        self.instance_global1 = nn.Sequential(
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+        )
+        self.instance_global2 = nn.Sequential(
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.Conv1d(128, 128, 1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+        )
+        self.instance_global3 = nn.Sequential(
             nn.Conv1d(128, 128, 1),
             nn.ReLU(),
             nn.Conv1d(128, 1024, 1),
@@ -109,37 +131,48 @@ class DeformNet(nn.Module):
         p0, p1, p2, out_img = self.psp(img)
         #print("out_img.shape:", out_img.shape)
         #out_img.shape: torch.Size([32, 32, 192, 192])
-
         di = out_img.size()[1]
         emb = out_img.view(bs, di, -1)
         choose = choose.unsqueeze(1).repeat(1, di, 1)
         emb = torch.gather(emb, 2, choose).contiguous()
-        emb = self.instance_color(emb)
 
         di2 = p2.size()[1]
         emb2 = p2.view(bs, di2, -1)
         choose2ori = torch.div(chooseori, 4)
         choose2 = choose2ori.type(torch.cuda.IntTensor).unsqueeze(1).repeat(1, di2, 1).type(torch.cuda.LongTensor)
         emb2 = torch.gather(emb2, 2, choose2).contiguous()
-        emb2 = self.instance_color2(emb2)
 
         di1 = p1.size()[1]
         emb1 = p1.view(bs, di1, -1)
         choose1ori = torch.div(choose2ori, 4)
         choose1 = choose1ori.type(torch.cuda.IntTensor).unsqueeze(1).repeat(1, di1, 1).type(torch.cuda.LongTensor)
         emb1 = torch.gather(emb1, 2, choose1).contiguous()
-        emb1 = self.instance_color1(emb1)
 
         di0 = p0.size()[1]
         emb0 = p0.view(bs, di0, -1)
         choose0 = torch.div(choose1ori, 4).type(torch.cuda.IntTensor).unsqueeze(1).repeat(1, di0, 1).type(torch.cuda.LongTensor)
         emb0 = torch.gather(emb0, 2, choose0).contiguous()
-        emb0 = self.instance_color0(emb0)
 
-        print("emb.shape:", emb0.shape)
-        print("emb0.shape:", emb1.shape)
-        print("emb1.shape:", emb2.shape)
-        print("emb2.shape:", emb.shape)
+        emb0 = self.instance_color0(emb0)
+        emb1 = self.instance_color1(emb1)
+        emb2 = self.instance_color2(emb2)
+        emb3 = self.instance_color(emb)
+
+        inst_local0 = torch.cat((points, emb0), dim=1)     # bs x 128 x n_pts
+        inst_global0 = self.instance_global0(inst_local0)    # bs x 1024 x 1
+
+        inst_local1 = torch.cat((points, emb1), dim=1)     # bs x 128 x n_pts
+        inst_local1 = inst_local1 + inst_global0
+        inst_global1 = self.instance_global1(inst_local1)    # bs x 1024 x 1
+
+        inst_local2 = torch.cat((points, emb2), dim=1)     # bs x 128 x n_pts
+        inst_local2 = inst_local2 + inst_global1
+        inst_global2 = self.instance_global2(inst_local2)    # bs x 1024 x 1
+
+        inst_local3 = torch.cat((points, emb3), dim=1)     # bs x 128 x n_pts
+        inst_local3 = inst_local3 + inst_global2
+        inst_global3 = self.instance_global3(inst_local3)    # bs x 1024 x 1
+        print("inst_global3.shape:", inst_global3.shape)
         #print("emb.shape:", emb.shape)
         #emb.shape: torch.Size([32, 32, 36864])
         #print("choose.shape:", choose.shape)
@@ -148,21 +181,19 @@ class DeformNet(nn.Module):
         #emb2.shape: torch.Size([32, 32, 1024])
         #print("emb3.shape:", emb.shape)
         #emb3.shape: torch.Size([32, 64, 1024])
-        inst_local = torch.cat((points, emb), dim=1)     # bs x 128 x n_pts
-        inst_global = self.instance_global(inst_local)    # bs x 1024 x 1
         # category-specific features
         cat_prior = prior.permute(0, 2, 1)
         cat_local = self.category_local(cat_prior)    # bs x 64 x n_pts
         cat_global = self.category_global(cat_local)  # bs x 1024 x 1
         # assignemnt matrix
-        assign_feat = torch.cat((inst_local, inst_global.repeat(1, 1, n_pts), cat_global.repeat(1, 1, n_pts)), dim=1)     # bs x 2176 x n_pts
+        assign_feat = torch.cat((inst_local3, inst_global3.repeat(1, 1, n_pts), cat_global.repeat(1, 1, n_pts)), dim=1)     # bs x 2176 x n_pts
         assign_mat = self.assignment(assign_feat)
         assign_mat = assign_mat.view(-1, nv, n_pts).contiguous()   # bs, nc*nv, n_pts -> bs*nc, nv, n_pts
         index = cat_id + torch.arange(bs, dtype=torch.long).cuda() * self.n_cat
         assign_mat = torch.index_select(assign_mat, 0, index)   # bs x nv x n_pts
         assign_mat = assign_mat.permute(0, 2, 1).contiguous()    # bs x n_pts x nv
         # deformation field
-        deform_feat = torch.cat((cat_local, cat_global.repeat(1, 1, nv), inst_global.repeat(1, 1, nv)), dim=1)       # bs x 2112 x n_pts
+        deform_feat = torch.cat((cat_local, cat_global.repeat(1, 1, nv), inst_global3.repeat(1, 1, nv)), dim=1)       # bs x 2112 x n_pts
         deltas = self.deformation(deform_feat)
         deltas = deltas.view(-1, 3, nv).contiguous()   # bs, nc*3, nv -> bs*nc, 3, nv
         deltas = torch.index_select(deltas, 0, index)   # bs x 3 x nv
