5d111b98e630d1d70bd84514cf4f455af8241d7d
JiazeWang <jiazewang@csu.edu.cn>
2020-10-12 23:17:09 +0800

    Revert "Update train_deform2.py"
    This reverts commit 09b2d2f8eafb083590f26ca520ef502c82e66e49.

--- train_deform2.py
+++ train_deform2.py
@@ -26,12 +26,12 @@ parser.add_argument('--num_workers', type=int, default=10, help='number of data 
 parser.add_argument('--gpu', type=str, default='0', help='GPU to use')
 parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate')
 parser.add_argument('--start_epoch', type=int, default=1, help='which epoch to start')
-parser.add_argument('--max_epoch', type=int, default=100, help='max number of epochs to train')
+parser.add_argument('--max_epoch', type=int, default=50, help='max number of epochs to train')
 parser.add_argument('--resume_model', type=str, default='', help='resume from saved model')
-parser.add_argument('--result_dir', type=str, default='results/camera_t2_100', help='directory to save train results')
+parser.add_argument('--result_dir', type=str, default='results/camera_t2', help='directory to save train results')
 opt = parser.parse_args()
 
-opt.decay_epoch = [0, 20, 40, 60, 80]
+opt.decay_epoch = [0, 10, 20, 30, 40]
 opt.decay_rate = [1.0, 0.6, 0.3, 0.1, 0.01]
 opt.corr_wt = 1.0
 opt.cd_wt = 5.0
