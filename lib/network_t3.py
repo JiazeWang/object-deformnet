104a5e4e9743c4fc0b25c15bd7646bd472e9e221
JiazeWang <jiazewang@csu.edu.cn>
2020-10-14 02:39:39 +0800

    200test

--- lib/network_t3.py
+++ lib/network_t3.py
@@ -247,8 +247,8 @@ class DeformNet(nn.Module):
         deltas1 = deltas1.view(-1, 3, nv).contiguous()   # bs, nc*3, nv -> bs*nc, 3, nv
         deltas1 = torch.index_select(deltas1, 0, index1)   # bs x 3 x nv
         deltas1 = deltas1.permute(0, 2, 1).contiguous()   # bs x nv x 3
-        assign_mat1 = torch.bmm(assign_mat0, assign_mat1.permute(0, 2, 1))
-        deltas1 = deltas0 + deltas1
+        #assign_mat1 = torch.bmm(assign_mat0, assign_mat1.permute(0, 2, 1))
+        #deltas1 = deltas0 + deltas1
 
 
 
@@ -264,8 +264,8 @@ class DeformNet(nn.Module):
         deltas2 = deltas2.view(-1, 3, nv).contiguous()   # bs, nc*3, nv -> bs*nc, 3, nv
         deltas2 = torch.index_select(deltas2, 0, index2)   # bs x 3 x nv
         deltas2 = deltas2.permute(0, 2, 1).contiguous()   # bs x nv x 3
-        assign_mat2 = torch.bmm(assign_mat1, assign_mat2.permute(0, 2, 1))
-        deltas2 = deltas1 + deltas2
+        #assign_mat2 = torch.bmm(assign_mat1, assign_mat2.permute(0, 2, 1))
+        #deltas2 = deltas1 + deltas2
 
         assign_feat3 = torch.cat((inst_local3, inst_global3.repeat(1, 1, n_pts), cat_global.repeat(1, 1, n_pts)), dim=1)     # bs x 2176 x n_pts
         assign_mat3 = self.assignment3(assign_feat3)
@@ -279,8 +279,8 @@ class DeformNet(nn.Module):
         deltas3 = deltas3.view(-1, 3, nv).contiguous()   # bs, nc*3, nv -> bs*nc, 3, nv
         deltas3 = torch.index_select(deltas3, 0, index3)   # bs x 3 x nv
         deltas3 = deltas3.permute(0, 2, 1).contiguous()   # bs x nv x 3
-        assign_mat3 = torch.bmm(assign_mat2, assign_mat3.permute(0, 2, 1))
-        deltas3 = deltas2 + deltas3
+        #assign_mat3 = torch.bmm(assign_mat2, assign_mat3.permute(0, 2, 1))
+        #deltas3 = deltas2 + deltas3
 
         # Loss calculation
         loss0, corr_loss0, cd_loss0, entropy_loss0, deform_loss0 = self.loss(assign_mat0, deltas0, prior, nocs, model)
--- train_deform3.py
+++ train_deform3.py
@@ -60,7 +60,7 @@ def train_net():
     val_dataset = PoseDataset(opt.dataset, 'test', opt.data_dir, opt.n_pts, opt.img_size)
     # start training
     st_time = time.time()
-    train_steps = 3000
+    train_steps = 200
     global_step = train_steps * (opt.start_epoch - 1)
     n_decays = len(opt.decay_epoch)
     assert len(opt.decay_rate) == n_decays
@@ -160,7 +160,7 @@ def train_net():
         easy_success = np.zeros((opt.n_cat,), dtype=int)      # 10 degree and 5 cm
         iou_success = np.zeros((opt.n_cat,), dtype=int)       # relative scale error < 0.1
         # sample validation subset
-        val_size = 3000
+        val_size = 200
         val_idx = random.sample(list(range(val_dataset.length)), val_size)
         val_sampler = torch.utils.data.sampler.SubsetRandomSampler(val_idx)
         #val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=1,
