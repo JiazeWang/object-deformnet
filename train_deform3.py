3d8074704a46930e58b6e9de1046b5836a18ea5e
JiazeWang <jiazewang@csu.edu.cn>
2020-10-14 03:51:23 +0800

    Update train_deform3.py

--- train_deform3.py
+++ train_deform3.py
@@ -60,7 +60,7 @@ def train_net():
     val_dataset = PoseDataset(opt.dataset, 'test', opt.data_dir, opt.n_pts, opt.img_size)
     # start training
     st_time = time.time()
-    train_steps = 200
+    train_steps = 3000
     global_step = train_steps * (opt.start_epoch - 1)
     n_decays = len(opt.decay_epoch)
     assert len(opt.decay_rate) == n_decays
