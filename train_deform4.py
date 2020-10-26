42a7cdcd8e323da6e89b5c492ee90f9395615b49
JiazeWang <jiazewang@csu.edu.cn>
2020-10-26 16:55:43 +0800

    Update train_deform4.py

--- train_deform4.py
+++ train_deform4.py
@@ -117,7 +117,8 @@ def train_net():
             prior = prior.cuda()
             sRT = sRT.cuda()
             nocs = nocs.cuda()
-            assign_mat3, deltas3, loss, corr_loss, cd_loss, entropy_loss, deform_loss = estimator(points, rgb, choose, cat_id, prior, nocs, model)
+            #assign_mat3, deltas3, loss, corr_loss, cd_loss, entropy_loss, deform_loss = estimator(points, rgb, choose, cat_id, prior, nocs, model)
+            assign_mat3, deltas3 = estimator(points, rgb, choose, cat_id, prior)
             """
             loss0, corr_loss0, cd_loss0, entropy_loss0, deform_loss0 = criterion(assign_mat0, deltas0, prior, nocs, model)
             loss1, corr_loss1, cd_loss1, entropy_loss1, deform_loss1 = criterion(assign_mat1, deltas1, prior, nocs, model)
