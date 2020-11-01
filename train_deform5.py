c9cf63d920a454a9bc7b94a86c13099cb0c09f1e
JiazeWang <jiazewang@csu.edu.cn>
2020-11-02 04:06:07 +0800

    t5

--- train_deform5.py
+++ train_deform5.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import tensorflow as tf
 #from lib.network_t4_psp import DeformNet
-from lib.network_t1_four import DeformNet
+from lib.network_t5 import DeformNet
 from lib.loss import Loss
 from data.pose_dataset import PoseDataset
 from lib.utils import setup_logger, compute_sRT_errors
