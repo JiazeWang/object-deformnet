7ddadc0df763a86da449a1427b4665a80899b21d
JiazeWang <jiazewang@csu.edu.cn>
2020-11-04 13:38:41 +0800

    Update train_deform5.py

--- train_deform5.py
+++ train_deform5.py
@@ -33,7 +33,7 @@ parser.add_argument('--lr', type=float, default=0.0001, help='initial learning r
 parser.add_argument('--start_epoch', type=int, default=1, help='which epoch to start')
 parser.add_argument('--max_epoch', type=int, default=50, help='max number of epochs to train')
 parser.add_argument('--resume_model', type=str, default='', help='resume from saved model')
-parser.add_argument('--result_dir', type=str, default='results/T5_r_three_stage_camera', help='directory to save train results')
+parser.add_argument('--result_dir', type=str, default='results/T5_r_right_three_stage_camera', help='directory to save train results')
 opt = parser.parse_args()
 
 opt.decay_epoch = [0, 10, 20, 30, 40]
