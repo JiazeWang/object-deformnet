3e59a4fe39a214cea24646201bb4cb4ceba7b6b4
JiazeWang <jiazewang@csu.edu.cn>
2020-11-06 00:47:41 +0800

    Update test_vis.py

--- TestPoseEstimation/test_vis.py
+++ TestPoseEstimation/test_vis.py
@@ -131,7 +131,7 @@ def single_detect(estimator, raw_rgb, depth, segmentation, savename):
         f_points_cpu = f_points.cpu()
         save_point_name = savename + str(i) +'_point.xyz'
         print(f_points_cpu.shape)
-        np.savetxt(save_point_name, f_points_cpu, fmt="%.6f")
+        np.savetxt(save_point_name, f_points_cpu[i], fmt="%.6f")
         assign_mat, deltas = estimator(f_points, f_rgb, f_choose, f_catId, f_prior)
         inst_shape = f_prior + deltas
         assign_mat = F.softmax(assign_mat, dim=2)
