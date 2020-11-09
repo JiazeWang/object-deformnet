6e77287284258ba0186e8bef8b2c81e145e49aef
JiazeWang <jiazewang@csu.edu.cn>
2020-11-09 12:27:57 +0800

    0.512

--- train_deform3.py
+++ train_deform3.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import tensorflow as tf
-from lib.network_t3_stage import DeformNet
+from lib.network_t3_r import DeformNet
 from lib.loss import Loss
 from data.pose_dataset import PoseDataset
 from lib.utils import setup_logger, compute_sRT_errors
