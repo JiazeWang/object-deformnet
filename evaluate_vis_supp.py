5ebdd421e0f4581a2e1c99318800e5d58f4f792d
JiazeWang <jiazewang@csu.edu.cn>
2020-11-21 18:31:51 +0800

    Update evaluate_vis_supp.py

--- evaluate_vis_supp.py
+++ evaluate_vis_supp.py
@@ -45,16 +45,71 @@ def evaluate():
     shift_thres_list = [i / 2 for i in range(21)]
     iou_thres_list = [i / 100 for i in range(101)]
     # predictions
-
-    result_dir = "vis"
+    result_dir = "results/final_transformers"
     #result_dir = 'results/eval_spd_real'
-    pkl_path = os.path.join('results/final_transformers/', 'mAP_Acc.pkl')
+    #result_dir = "results/eval_T5_f_STAGE3_R_CAMERA_2_1_0.5/"
+    result_pkl_list = glob.glob(os.path.join(result_dir, 'results_*.pkl'))
+    result_pkl_list = sorted(result_pkl_list)
+    assert len(result_pkl_list)
+    pred_results = []
+    for pkl_path in result_pkl_list:
+        with open(pkl_path, 'rb') as f:
+            result = cPickle.load(f)
+            if 'gt_handle_visibility' not in result:
+                result['gt_handle_visibility'] = np.ones_like(result['gt_class_ids'])
+            else:
+                assert len(result['gt_handle_visibility']) == len(result['gt_class_ids']), "{} {}".format(
+                    result['gt_handle_visibility'], result['gt_class_ids'])
+        if type(result) is list:
+            pred_results += result
+        elif type(result) is dict:
+            pred_results.append(result)
+        else:
+            assert False
+    # To be consistent with NOCS, set use_matches_for_pose=True for mAP evaluation
+    iou_aps, pose_aps, iou_acc, pose_acc = compute_mAP(pred_results, result_dir, degree_thres_list, shift_thres_list,
+                                                       iou_thres_list, iou_pose_thres=0.1, use_matches_for_pose=True)
+    # metric
+    fw = open('{0}/eval_logs.txt'.format(result_dir), 'a')
+    iou_25_idx = iou_thres_list.index(0.25)
+    iou_50_idx = iou_thres_list.index(0.5)
+    iou_75_idx = iou_thres_list.index(0.75)
+    degree_05_idx = degree_thres_list.index(5)
+    degree_10_idx = degree_thres_list.index(10)
+    shift_02_idx = shift_thres_list.index(2)
+    shift_05_idx = shift_thres_list.index(5)
+    messages = []
+    messages.append('mAP:')
+    messages.append('3D IoU at 25: {:.1f}'.format(iou_aps[-1, iou_25_idx] * 100))
+    messages.append('3D IoU at 50: {:.1f}'.format(iou_aps[-1, iou_50_idx] * 100))
+    messages.append('3D IoU at 75: {:.1f}'.format(iou_aps[-1, iou_75_idx] * 100))
+    messages.append('5 degree, 2cm: {:.1f}'.format(pose_aps[-1, degree_05_idx, shift_02_idx] * 100))
+    messages.append('5 degree, 5cm: {:.1f}'.format(pose_aps[-1, degree_05_idx, shift_05_idx] * 100))
+    messages.append('10 degree, 2cm: {:.1f}'.format(pose_aps[-1, degree_10_idx, shift_02_idx] * 100))
+    messages.append('10 degree, 5cm: {:.1f}'.format(pose_aps[-1, degree_10_idx, shift_05_idx] * 100))
+    messages.append('Acc:')
+    messages.append('3D IoU at 25: {:.1f}'.format(iou_acc[-1, iou_25_idx] * 100))
+    messages.append('3D IoU at 50: {:.1f}'.format(iou_acc[-1, iou_50_idx] * 100))
+    messages.append('3D IoU at 75: {:.1f}'.format(iou_acc[-1, iou_75_idx] * 100))
+    messages.append('5 degree, 2cm: {:.1f}'.format(pose_acc[-1, degree_05_idx, shift_02_idx] * 100))
+    messages.append('5 degree, 5cm: {:.1f}'.format(pose_acc[-1, degree_05_idx, shift_05_idx] * 100))
+    messages.append('10 degree, 2cm: {:.1f}'.format(pose_acc[-1, degree_10_idx, shift_02_idx] * 100))
+    messages.append('10 degree, 5cm: {:.1f}'.format(pose_acc[-1, degree_10_idx, shift_05_idx] * 100))
+    for msg in messages:
+        print(msg)
+        fw.write(msg + '\n')
+    fw.close()
+    # load NOCS results
+    #pkl_path = os.path.join('results/nocs_results', opt.data, 'mAP_Acc.pkl')
+    """
+    pkl_path = os.path.join('results/eval_spd_real/', 'mAP_Acc.pkl')
     with open(pkl_path, 'rb') as f:
         nocs_results = cPickle.load(f)
-    iou_aps = nocs_results['iou_aps'][-1, :]
-    pose_aps = nocs_results['pose_aps'][-1, :, :]
-    #iou_aps = np.concatenate((iou_aps, nocs_iou_aps[None, :]), axis=0)
-    #pose_aps = np.concatenate((pose_aps, nocs_pose_aps[None, :, :]), axis=0)
+    nocs_iou_aps = nocs_results['iou_aps'][-1, :]
+    nocs_pose_aps = nocs_results['pose_aps'][-1, :, :]
+    iou_aps = np.concatenate((iou_aps, nocs_iou_aps[None, :]), axis=0)
+    pose_aps = np.concatenate((pose_aps, nocs_pose_aps[None, :, :]), axis=0)
+    """
     # plot
     plot_mAP2(iou_aps, pose_aps, result_dir, iou_thres_list, degree_thres_list, shift_thres_list)
 
