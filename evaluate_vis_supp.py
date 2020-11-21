53167567d420aa0bb44b66fff5c22f8feeba9111
JiazeWang <jiazewang@csu.edu.cn>
2020-11-21 20:18:45 +0800

    supp

--- evaluate_vis_supp.py
+++ evaluate_vis_supp.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm import tqdm
 import _pickle as cPickle
 from lib.align import estimateSimilarityTransform
-from lib.utils import load_depth, get_bbox, compute_mAP, plot_mAP2
+from lib.utils import load_depth, get_bbox, compute_mAP, plot_mAP3
 
 
 parser = argparse.ArgumentParser()
@@ -41,6 +41,7 @@ ymap = np.array([[j for i in range(640)] for j in range(480)])
 
 
 def evaluate():
+    """
     degree_thres_list = list(range(0, 61, 1))
     shift_thres_list = [i / 2 for i in range(21)]
     iou_thres_list = [i / 100 for i in range(101)]
@@ -70,52 +71,32 @@ def evaluate():
     iou_aps, pose_aps, iou_acc, pose_acc = compute_mAP(pred_results, result_dir, degree_thres_list, shift_thres_list,
                                                        iou_thres_list, iou_pose_thres=0.1, use_matches_for_pose=True)
     # metric
-    fw = open('{0}/eval_logs.txt'.format(result_dir), 'a')
-    iou_25_idx = iou_thres_list.index(0.25)
-    iou_50_idx = iou_thres_list.index(0.5)
-    iou_75_idx = iou_thres_list.index(0.75)
-    degree_05_idx = degree_thres_list.index(5)
-    degree_10_idx = degree_thres_list.index(10)
-    shift_02_idx = shift_thres_list.index(2)
-    shift_05_idx = shift_thres_list.index(5)
-    messages = []
-    messages.append('mAP:')
-    messages.append('3D IoU at 25: {:.1f}'.format(iou_aps[-1, iou_25_idx] * 100))
-    messages.append('3D IoU at 50: {:.1f}'.format(iou_aps[-1, iou_50_idx] * 100))
-    messages.append('3D IoU at 75: {:.1f}'.format(iou_aps[-1, iou_75_idx] * 100))
-    messages.append('5 degree, 2cm: {:.1f}'.format(pose_aps[-1, degree_05_idx, shift_02_idx] * 100))
-    messages.append('5 degree, 5cm: {:.1f}'.format(pose_aps[-1, degree_05_idx, shift_05_idx] * 100))
-    messages.append('10 degree, 2cm: {:.1f}'.format(pose_aps[-1, degree_10_idx, shift_02_idx] * 100))
-    messages.append('10 degree, 5cm: {:.1f}'.format(pose_aps[-1, degree_10_idx, shift_05_idx] * 100))
-    messages.append('Acc:')
-    messages.append('3D IoU at 25: {:.1f}'.format(iou_acc[-1, iou_25_idx] * 100))
-    messages.append('3D IoU at 50: {:.1f}'.format(iou_acc[-1, iou_50_idx] * 100))
-    messages.append('3D IoU at 75: {:.1f}'.format(iou_acc[-1, iou_75_idx] * 100))
-    messages.append('5 degree, 2cm: {:.1f}'.format(pose_acc[-1, degree_05_idx, shift_02_idx] * 100))
-    messages.append('5 degree, 5cm: {:.1f}'.format(pose_acc[-1, degree_05_idx, shift_05_idx] * 100))
-    messages.append('10 degree, 2cm: {:.1f}'.format(pose_acc[-1, degree_10_idx, shift_02_idx] * 100))
-    messages.append('10 degree, 5cm: {:.1f}'.format(pose_acc[-1, degree_10_idx, shift_05_idx] * 100))
-    for msg in messages:
-        print(msg)
-        fw.write(msg + '\n')
-    fw.close()
     # load NOCS results
     #pkl_path = os.path.join('results/nocs_results', opt.data, 'mAP_Acc.pkl')
     """
+
+
+
     pkl_path = os.path.join('results/eval_spd_real/', 'mAP_Acc.pkl')
     with open(pkl_path, 'rb') as f:
         nocs_results = cPickle.load(f)
-    nocs_iou_aps = nocs_results['iou_aps'][-1, :]
-    nocs_pose_aps = nocs_results['pose_aps'][-1, :, :]
-    iou_aps = np.concatenate((iou_aps, nocs_iou_aps[None, :]), axis=0)
-    pose_aps = np.concatenate((pose_aps, nocs_pose_aps[None, :, :]), axis=0)
-    """
-    # plot
-    plot_mAP2(iou_aps, pose_aps, result_dir, iou_thres_list, degree_thres_list, shift_thres_list)
+    nocs_iou_aps = nocs_results['iou_aps'][1, :]
+    nocs_pose_aps = nocs_results['pose_aps'][1, :, :]
+
+    pkl_path_new = os.path.join('results/eval_spd_real/', 'mAP_Acc.pkl')
+    with open(pkl_path_new, 'rb') as f:
+        nocs_results_new = cPickle.load(f)
+    nocs_iou_aps_new = nocs_results_new['iou_aps'][1, :]
+    nocs_pose_aps_new = nocs_results_new['pose_aps'][1, :, :]
+
+
+    iou_aps = np.concatenate((nocs_iou_aps, nocs_pose_aps_new), axis=0)
+    pose_aps = np.concatenate((nocs_pose_aps, nocs_pose_aps), axis=0)
+
+
+    plot_mAP3(iou_aps, pose_aps, result_dir, iou_thres_list, degree_thres_list, shift_thres_list, name = 'first')
 
 
 if __name__ == '__main__':
-    #print('Detecting ...')
-    #detect()
     print('Evaluating ...')
     evaluate()
